5574bbcc8a120cbd18a0562049c969cd11ad2ccf
abdalrahmanshaip <abdalrahmanshaip@gmail.com>
2025-01-20 21:04:40 +0200

    - Add aria for accessibility
    - Add color to scroll bar

--- src/components/features/userData/_components/UserForm.tsx
+++ src/components/features/userData/_components/UserForm.tsx
@@ -14,6 +14,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useTranslations } from 'next-intl'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+
 const UserForm = () => {
   const t = useTranslations('form')
   const form = useForm<z.infer<typeof userSchema>>({
@@ -29,30 +30,46 @@ const UserForm = () => {
   const onSubmit = (value: z.infer<typeof userSchema>) => {
     console.log(value)
   }
+
   return (
     <div className='w-full'>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className='lg:space-y-8 space-y-6'
+          aria-labelledby='form-title'
         >
-          <div className='flex flex-col lg:flex-row  gap-8 '>
+          <h1
+            id='form-title'
+            className='sr-only'
+          >
+            User form
+          </h1>
+          <div className='flex flex-col lg:flex-row gap-8'>
             <FormField
               control={form.control}
               name='firstname'
               render={({ field }) => (
                 <FormItem className='flex-1'>
-                  <FormLabel className='font-medium text-sm text-our-dark'>
+                  <FormLabel
+                    className='font-medium text-sm text-our-dark'
+                    htmlFor='firstname'
+                  >
                     {t('firstName')}
                   </FormLabel>
                   <FormControl>
                     <Input
+                      id='firstname'
                       placeholder={t('firstName')}
                       {...field}
-                      className='bg-white lg:py-6 py-5 ps-6 rounded-[2px] '
+                      className='bg-white lg:py-6 py-5 ps-6 rounded-[2px]'
+                      aria-invalid={!!form.formState.errors.firstname}
+                      aria-describedby='firstname-error'
                     />
                   </FormControl>
-                  <FormMessage />
+                  <FormMessage id='firstname-error'>
+                    {form.formState.errors.firstname?.message}
+                  </FormMessage>
                 </FormItem>
               )}
             />
@@ -61,17 +78,25 @@ const UserForm = () => {
               name='lastname'
               render={({ field }) => (
                 <FormItem className='flex-1'>
-                  <FormLabel className='font-medium text-sm text-our-dark'>
+                  <FormLabel
+                    className='font-medium text-sm text-our-dark'
+                    htmlFor='lastname'
+                  >
                     {t('lastName')}
                   </FormLabel>
                   <FormControl>
                     <Input
+                      id='lastname'
                       placeholder={t('lastName')}
                       {...field}
                       className='bg-white lg:py-6 py-5 ps-6 rounded-[2px]'
+                      aria-invalid={!!form.formState.errors.lastname}
+                      aria-describedby='lastname-error'
                     />
                   </FormControl>
-                  <FormMessage />
+                  <FormMessage id='lastname-error'>
+                    {form.formState.errors.lastname?.message}
+                  </FormMessage>
                 </FormItem>
               )}
             />
@@ -81,17 +106,25 @@ const UserForm = () => {
             name='mobileNumber'
             render={({ field }) => (
               <FormItem>
-                <FormLabel className='font-medium text-sm text-our-dark'>
+                <FormLabel
+                  className='font-medium text-sm text-our-dark'
+                  htmlFor='mobileNumber'
+                >
                   {t('mobileNumber')}
                 </FormLabel>
                 <FormControl>
                   <Input
+                    id='mobileNumber'
                     placeholder={t('mobileNumber')}
                     {...field}
                     className='bg-white lg:py-6 py-5 ps-6 rounded-[2px]'
+                    aria-invalid={!!form.formState.errors.mobileNumber}
+                    aria-describedby='mobileNumber-error'
                   />
                 </FormControl>
-                <FormMessage />
+                <FormMessage id='mobileNumber-error'>
+                  {form.formState.errors.mobileNumber?.message}
+                </FormMessage>
               </FormItem>
             )}
           />
@@ -100,23 +133,32 @@ const UserForm = () => {
             name='email'
             render={({ field }) => (
               <FormItem>
-                <FormLabel className='font-medium text-sm text-our-dark'>
+                <FormLabel
+                  className='font-medium text-sm text-our-dark'
+                  htmlFor='email'
+                >
                   {t('email')}
                 </FormLabel>
                 <FormControl>
                   <Input
+                    id='email'
                     placeholder={t('email')}
                     {...field}
                     className='bg-white lg:py-6 py-5 ps-6 rounded-[2px]'
+                    aria-invalid={!!form.formState.errors.email}
+                    aria-describedby='email-error'
                   />
                 </FormControl>
-                <FormMessage />
+                <FormMessage id='email-error'>
+                  {form.formState.errors.email?.message}
+                </FormMessage>
               </FormItem>
             )}
           />
           <Button
             type='submit'
-            className='w-full  py-2.5 '
+            className='w-full py-2.5'
+            aria-label={t('button')}
           >
             {t('button')}
           </Button>
