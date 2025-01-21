a2b6cfffca3ea076a87dba9ab019ce0821172ce3
abdalrahmanshaip <abdalrahmanshaip@gmail.com>
2025-01-22 00:30:51 +0200

    -m Handle style like figma design
    - Add coordinates of digifly company

--- src/components/features/map/_component/MapContent.tsx
+++ src/components/features/map/_component/MapContent.tsx
@@ -1,22 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
 import L from 'leaflet'
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 import 'leaflet/dist/leaflet.css'
-import { useTranslations } from 'next-intl'
+import PopupMessage from './PopupMessage'
 
 const MapContent = () => {
-  const t = useTranslations('lableMap')
   const DefaultIcon = L.icon({
     iconUrl: icon as any,
     shadowUrl: iconShadow as any,
   })
   return (
     <MapContainer
-      center={[30.061728, 31.335321]}
+      center={[30.061736656473236, 31.336938759513913]}
       zoom={50}
       scrollWheelZoom={false}
     >
@@ -25,18 +24,10 @@ const MapContent = () => {
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <Marker
-        position={[30.061728, 31.335321]}
+        position={[30.061736656473236, 31.336938759513913]}
         icon={DefaultIcon}
       >
-        <Popup>
-          <div className='!text-white !font-bold !text-base !p-2'>
-            <div className=''>
-              <span className='!text-our-green'>{t('specialTitle')}</span>
-              <span>{t('firstTitle')}</span>
-            </div>
-            <span>{t('secTitle')}</span>
-          </div>
-        </Popup>
+        <PopupMessage />
       </Marker>
     </MapContainer>
   )
