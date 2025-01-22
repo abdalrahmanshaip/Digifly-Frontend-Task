60a9595a0add0518102092cd4494f0354783688c
abdalrahmanshaip <abdalrahmanshaip@gmail.com>
2025-01-22 11:27:01 +0200

    Remove Error from console

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,8 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export default function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/api')) {
-    return NextResponse.next()
-  }
-
-  return createMiddleware(routing)(request)
-}
+export default createMiddleware(routing)
 
 export const config = {
-  matcher: ['/', '/(ar|en)/:path*', '/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/', '/(ar|en)/:path*'],
 }
