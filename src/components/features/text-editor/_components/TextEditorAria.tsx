686d18a3bfa8a56329c7aa7358bfa03e49134f8d
abdalrahmanshaip <abdalrahmanshaip@gmail.com>
2025-01-22 07:13:07 +0200

    - Add Actions to Text Editior
    - Add ToolTip to button for show button for what when hover on it
    
    - Add 9 properties of Text editor
    
    1- Undo 2- Redo 3- Bold 4- Align Right 5- Align Center 6- Align Left 7- Underline 8- Italic 9- strikethrough

--- src/components/features/text-editor/_components/TextEditorAria.tsx
+++ src/components/features/text-editor/_components/TextEditorAria.tsx
@@ -1,4 +1,12 @@
 import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
@@ -9,8 +17,10 @@ import {
   AlignLeft,
   AlignRight,
   Bold,
-  ListOrdered,
+  Italic,
   Redo,
+  Strikethrough,
+  Underline,
   Undo,
 } from 'lucide-react'
 
@@ -18,50 +28,77 @@ const TextEditorAria = () => {
   const textEditorProperties = [
     {
       name: 'undo',
-      icon: <Undo />,
+      icon: <Undo size={20} />,
       action: () => document.execCommand('undo', false, ''),
     },
     {
       name: 'redo',
-      icon: <Redo />,
+      icon: <Redo size={20} />,
       action: () => document.execCommand('redo', false, ''),
     },
     {
       name: 'Bold',
-      icon: <Bold />,
+      icon: <Bold size={20} />,
       action: () => document.execCommand('Bold', false, ''),
     },
     {
       name: 'Align Right',
-      icon: <AlignRight />,
+      icon: <AlignRight size={20} />,
       action: () => document.execCommand('JustifyRight', false, ''),
     },
     {
       name: 'Align Center',
-      icon: <AlignCenter />,
+      icon: <AlignCenter size={20} />,
       action: () => document.execCommand('JustifyCenter', false, ''),
     },
     {
       name: 'Align Left',
-      icon: <AlignLeft />,
+      icon: <AlignLeft size={20} />,
       action: () => document.execCommand('JustifyLeft', false, ''),
     },
     {
-      name: 'Order List',
-      icon: <ListOrdered />,
-      action: () => document.execCommand('insertOrderedList', false, ''),
+      name: 'Underline',
+      icon: <Underline size={20} />,
+      action: () => document.execCommand('underline', false, ''),
+    },
+    {
+      name: 'Italic',
+      icon: <Italic size={20} />,
+      action: () => document.execCommand('italic', false, ''),
+    },
+    {
+      name: 'strikethrough',
+      icon: <Strikethrough size={20} />,
+      action: () => document.execCommand('strikeThrough', false, ''),
     },
   ]
   return (
     <div className='border border-gray-300 rounded-sm min-h-80 focus:outline-none bg-our-purple-100 overflow-hidden'>
       <TooltipProvider>
-        <div className='flex items-center bg-our-purple-400'>
+        <div className='flex items-center bg-our-purple-400 border-b border-our-purple/10'>
+          <Select
+            onValueChange={(value) =>
+              document.execCommand('fontName', false, value)
+            }
+          >
+            <SelectTrigger className='w-[180px] h-full rounded-none border-none focus:ring-0 py-4 px-6 border-our-purple/10'>
+              <SelectValue placeholder='Select font' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value='Monospace'>Monospace</SelectItem>
+                <SelectItem value='Sans Serif'>Sans Serif</SelectItem>
+                <SelectItem value='Arial'>Arial</SelectItem>
+                <SelectItem value='tajawal'>tajawal</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
           {textEditorProperties.map((property) => (
             <Tooltip key={property.name}>
               <TooltipTrigger asChild>
                 <button
                   onClick={property.action}
-                  className='border-x py-4 px-6 border-our-purple/10'
+                  className='border-x py-4 px-6 border-our-purple/10 hover:bg-our-green hover:text-white duration-300 '
                   role='button'
                 >
                   {property.icon}
@@ -76,6 +113,7 @@ const TextEditorAria = () => {
       </TooltipProvider>
       <div
         contentEditable
+        spellCheck='false'
         className='p-6  focus:outline-none  h-full'
       ></div>
     </div>
@@ -83,5 +121,3 @@ const TextEditorAria = () => {
 }
 
 export default TextEditorAria
-
-//  onChange={(e) => document.execCommand('fontName', false, e.target.value)}
